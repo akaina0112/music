4f5b1ad4d892d00f4f7074a99d71e3eb28811e6e
ライフガード <162636336+akaina0112@users.noreply.github.com>
2026-02-24 07:28:51 +0900

    list.js を更新

--- js/list.js
+++ js/list.js
@@ -1,1 +1,26 @@
-(
+const links = {
+  "row1": {
+    url: "https://www.youtube.com/watch?v=OMp9s9f4cnI",
+    description: "<span class='n'><h2>君と出会えた奇跡 / ライフガード</h2></span><h3><span class='n'>この曲は出会えたことに感謝していることを表しています。</span><span class='n'>君にというのは出逢えた人たちの事です。</span><span class='n'>ちなみに出逢えたは本来出会えたです。わざと出逢えたにしております。</span><span class='n'><span class='highlight'>公開日:2024/09/04</span></span></h3>"
+  },
+  "row2": {
+    url: "https://www.youtube.com/watch?v=AkYCR-N9hbY",
+    description: "<span class='n'><h2>君の笑顔に心が奪われる</h2></span><h3><span class='n'>この曲は恋愛が元になっております。</span><span class='n'>私は男性女性関係なく恋愛します。</span><span class='n'>笑顔に心が奪われるは見惚れてしまうということです。</span><span class='n'><span class='highlight'>公開日:2024/09/17</span></span></h3>"
+  },
+  "row3": {
+    url: "https://www.youtube.com/watch?v=i1jS16w7-18",
+    description: "<span class='n'><h2>あなたに会えるまで</h2></span><h3><span class='n'>この曲は私の現状が元になっております。</span><span class='n'>私はネットやリアル関係なく会えるまでという言葉で表しています。</span><span class='n'>人生は何があるか誰にもわからないから人みんな仲良くしたいなという気持ちです。三日後に公開しましたが作成したのは<span class='colorred'>君の笑顔に心が奪われる</span>の前です。</span><span class='n'><span class='highlight'>公開日:2024/09/20</span></span></h3>"
+  },
+  "row4": {
+    url: "https://www.youtube.com/watch?v=pMHAWqqdGXI",
+    description: "<span class='n'><h2>男の子らしく女の子らしく</h2></span><h3><span class='n'>この曲は私の周りの人が関係しております。</span><span class='n'>女の子なのに男の子みたいな子男の子なのに女の子みたいな人</span><span class='n'>けれど私はそれでいいやんと思いましたがキモイとか言ってる人がいたり<span class='n'>とある家族の親が女の子だから男の子だからと言っている姿を見たことがありイラっとしました。<span class='n'>女の子とか男の子とか関係なく自分らしく生きればいいのにね。</span></span></span><span class='n'><span class='highlight'>公開日:2024/09/24</span></span></h3>"
+  },
+  "row5": {
+    url: "https://www.youtube.com/watch?v=TUJX3bgp-jA",
+    description: "<span class='n'><h2>私はあなたは</h2></span><h3><span class='n'>この曲は私の元大切な人が関係しております。</span><span class='n'>私は人間関係が嫌いです。</span><span class='n'>嘘ばっかなので嫌になりました。</span><span class='n'>この曲は私の人生や周りの人に人生を表しています。</span><span class='n'><span class='highlight'>公開日:2024/10/03</span></span></h3>"
+  },
+  "row6": {
+    url: "https://www.youtube.com/watch?v=DOIXVXFHeTw",
+    description: "<span class='n'><h2>黒い約束</h2></span><h3><span class='n'>この曲は裏切りが元になった曲です。</span><span class='n'>小2から現在まで裏切られることが多くそれを歌詞にした。</span><span class='n'>まりまるさんと妹とるりぃとたく兄は今のところ裏切ってきていません。</span><span class='n'><a href=\"https://akaina0112.github.io/music/open/black.html\">続きを読む</a></span><span class='n'><span class='highlight'>公開日:2025/06/06</span></span></h3>"
+  }
+};
